b7efe5b3e9e0d79834c22241f52c679dc1f2783e
bita-tipi <107231389+bita-tipi@users.noreply.github.com>
2022-07-13 13:06:58 +0900

    added_routes

---
+++ src/routes/top.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./top.css";
+
+
+function Top() {
+    return (
+        <div className="main">
+        </div>
+    );
+}
+
+export default Top;
