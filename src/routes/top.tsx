3db947db3df66fcf79058f3fbf1124f092afa36f
bita-tipi <107231389+bita-tipi@users.noreply.github.com>
2022-07-14 16:16:42 +0900

    zenysai

--- src/routes/top.tsx
+++ src/routes/top.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import logo from "../logo.svg";
 import top_center from "../assets/img/top_center.svg"
 import top_link from "../assets/img/top_link.svg"
+import schedule from "../assets/img/schedule.svg"
+import obi from "../assets/img/2022.svg"
 import './top.css';
 import { Link, NavLink, useHistory } from "react-router-dom";
 
@@ -14,8 +16,12 @@ function Top() {
             <header className="App-header">
                 <div className="top_container">
                 <img src={top_center} className="top_center"/>
+                <Link to={`/top_back`}>
+                    <img src={schedule} className="schedule"/>
+                </Link>
                 <Link to={`/contents`}>
                     <img src={top_link} className="top_link"/>
+                <img src={obi} className="obi"/>
                 </Link>
                 </div>
                 
