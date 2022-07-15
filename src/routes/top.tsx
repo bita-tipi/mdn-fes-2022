7802bbea802e8ecba961af5d32ac4203e68fb437
SatooRu <ryuto65536@gmail.com>
2022-07-15 16:47:26 +0900

    topページのcssなど 変更した

--- src/routes/top.tsx
+++ src/routes/top.tsx
@@ -5,25 +5,27 @@ import top_center from "../assets/img/top_center.svg"
 import top_link from "../assets/img/top_link.svg"
 import schedule from "../assets/img/schedule.svg"
 import obi from "../assets/img/2022.svg"
-import './top.css';
+import "./top.css";
 import { Link, NavLink, useHistory } from "react-router-dom";
+
 
 function Top() {
     return (
         <div className="App">
-            <header className="App-header">
-                <div className="top_container">
-                <img src={top_center} className="top_center"/>
-                <Link to={`/top_back`}>
-                    <img src={schedule} className="schedule"/>
-                </Link>
-                <Link to={`/contents`}>
-                    <img src={top_link} className="top_link"/>
-                <img src={obi} className="obi"/>
-                </Link>
+            <div className="top_container">
+                <div className="top_center">
+                    <img src={top_center} />
+                    <Link to={"/top_back"} className="schedule">
+                        <img src={schedule} />
+                    </Link>
+                    <Link to={"/contents"} className="top_link">
+                        <img src={top_link} />
+                    </Link>
                 </div>
-                
-            </header>
+                <div className="obi">
+                    <img src={obi} />
+                </div>
+            </div>
         </div>
     );
 }
