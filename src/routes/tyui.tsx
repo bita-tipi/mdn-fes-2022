83c1ad0218d3dc245c46f486bef832ebef0af7f2
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-07-20 13:53:41 +0900

    tyuiiii

--- src/routes/tyui.tsx
+++ src/routes/tyui.tsx
@@ -1,24 +1,20 @@
 import { url } from "inspector";
 import React from "react";
-import logo from "../logo.svg";
-import top_center from "../assets/img/top_center.svg"
-import top_link from "../assets/img/top_link.svg"
-import schedule from "../assets/img/schedule.svg"
 import left_over from "../assets/img/left_over.svg"
 import back from "../assets/img/tyui_background.svg"
-import './tyui.css';
+import "./tyui.css";
 import { Link, NavLink, useHistory } from "react-router-dom";
-
-
 
 function Tyui() {
     return (
-        <div className="App">
-            <header className=".background_tyui">
+    <div>
+        <header className="background_tyui">
+            <div>
+            <img src={back} className="back"/>   
             <img src={left_over} className="left_over"/>
-            </header>
-
-        </div>
+            </div>
+        </header>
+    </div>
     );
 }
 
