7f44015fe55cee69bc49018937ed5db35ea350e0
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-07-20 15:06:45 +0900

    tyui6

--- src/routes/tyui.tsx
+++ src/routes/tyui.tsx
@@ -2,6 +2,9 @@ import { url } from "inspector";
 import React from "react";
 import left_over from "../assets/img/left_over.svg"
 import back from "../assets/img/tyui_background.svg"
+import right_over from "../assets/img/right_over.svg"
+import left_bottom from "../assets/img/tyui_left_bottom.svg"
+import right_bottom from "../assets/img/tyui_right_bottom.svg"
 import "./tyui.css";
 import { Link, NavLink, useHistory } from "react-router-dom";
 
@@ -13,6 +16,9 @@ function Tyui() {
             <img src={back} className="back"/>
             <img src={back} className="back2"/>      
             <img src={left_over} className="left_over"/>
+            <img src={right_over} className="right_over"/>
+            <img src={left_bottom} className="left_bottom"/>
+            <img src={right_bottom} className="right_bottom"/>
             </div>
         </header>
     </div>
