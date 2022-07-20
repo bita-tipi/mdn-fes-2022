76e9923424a368a936c7f8e7dd67dc7bad3b170b
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-07-20 15:16:12 +0900

    tyui8

--- src/routes/tyui.tsx
+++ src/routes/tyui.tsx
@@ -2,7 +2,7 @@ import { url } from "inspector";
 import React from "react";
 import left_over from "../assets/img/left_over.svg"
 import back from "../assets/img/tyui_background.svg"
-import right_over from "../assets/img/right_over.svg"
+import right_over from "../assets/img/right_over2.svg"
 import left_bottom from "../assets/img/tyui_left_bottom.svg"
 import right_bottom from "../assets/img/tyui_right_bottom.svg"
 import "./tyui.css";
