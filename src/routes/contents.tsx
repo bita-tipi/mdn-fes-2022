f9b5c962d8284904265f66248e32ef02133b6c92
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-07-20 22:04:22 +0900

    atyui9

--- src/routes/contents.tsx
+++ src/routes/contents.tsx
@@ -2,7 +2,8 @@ import { url } from "inspector";
 import React from "react";
 import logo from "../logo.svg";
 import top_center from "../images/top_center.svg"
-import contents from "../assets/img/contents_tyui.png"
+import contents from "../assets/img/contents_tyui.svg"
+import contents2 from "../assets/img/contents_map.svg"
 import './contents.css';
 import { Link, NavLink, useHistory } from "react-router-dom";
 
@@ -15,6 +16,9 @@ function Contents() {
             <header className="background_contents">
             <Link to={`/tyui`}>
                     <img src={contents} className="contents_ tyui"/>
+                </Link>
+            <Link to={`/map`}>
+                    <img src={contents2} className="contents_ map"/>
                 </Link>
 
             </header>
