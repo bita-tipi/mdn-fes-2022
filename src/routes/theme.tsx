7e618e545c0ed85798aff0ff802360a0d7ff26a5
bitter@gmail.com <bitter@gmail.com>
2022-08-13 19:21:00 +0900

    theme

--- src/routes/theme.tsx
+++ src/routes/theme.tsx
@@ -3,15 +3,19 @@ import React from "react";
 import Header from "./headermain"
 import "./theme.css"
 import { Link, NavLink, useHistory } from "react-router-dom"
-
+import main from "../assets/img/theme_main.svg"
+import midasi from "../assets/img/theme_midasi.svg"
+import MV from "../assets/img/theme_MV.svg"
 
 function Theme() {
     return (
-        <body className="top_container">
-            <Header/>
+        <body className="background_theme">
             <div>
-
+            <img src={midasi} className="midasi"/>
+            <img src={main} className="main"/>
+            <img src={MV} className="MV"/>
             </div>
+            <Header/>
         </body>
     );
 }
