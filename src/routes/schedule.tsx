83d42f799559ebe7ab0f7ac6fc664c60b05ff818
bitter@gmail.com <bitter@gmail.com>
2022-08-16 00:13:39 +0900

    schedule

--- src/routes/schedule.tsx
+++ src/routes/schedule.tsx
@@ -3,13 +3,19 @@ import React from "react";
 import Header from "./headermain"
 import "./schedule.css"
 import { Link, NavLink, useHistory } from "react-router-dom"
-
+import main_bar from "../assets/img/main_bar.png"
+import side_bar from "../assets/img/side_bar.png"
+import time_bar from "../assets/img/time_bar.svg"
+import time from "../assets/img/time.png"
 
 function Schedule() {
     return (
         <body className="background_schedule">
             <div>
-
+            <img src={main_bar} className="main_bar"/>
+            <img src={side_bar} className="side_bar"/>
+            <img src={time_bar} className="time_bar"/>
+            <img src={time} className="time"/>
             </div>
             <Header/>
         </body>
