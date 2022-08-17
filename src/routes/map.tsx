dc21ff510a88279a4b4a2f16a34de0befa6f90f8
bitter@gmail.com <bitter@gmail.com>
2022-08-18 00:05:03 +0900

    map if

--- src/routes/map.tsx
+++ src/routes/map.tsx
@@ -1,6 +1,6 @@
 import { url } from "inspector";
 import React from "react";
-import logo from "../logo.svg";
+import {useState} from "react";
 import map_list from "../assets/img/map_list.svg"
 import './map.css';
 import { Link, NavLink, useHistory } from "react-router-dom";
@@ -14,14 +14,59 @@ import b_3 from "../assets/img/map_3_b.svg"
 import y_4 from "../assets/img/map_4_y.svg"
 import b_4 from "../assets/img/map_4_b.svg"
 
+
 function Map() {
+    const [G1] = useState(true);
+    const [G3] = useState(false);
+    const [G2] = useState(false);
+    const [G4] = useState(false);
+    function s_1(){
+        const G1=true;
+        const G2=false;
+        const G3=false;
+        const G4=false;
+    }
+    function s_2(){
+        const G1=false;
+        const G2=true;
+        const G3=false;
+        const G4=false;
+    }
+    function s_3(){
+        const G1=false;
+        const G2=false;
+        const G3=true;
+        const G4=false;
+    }
+    function s_4(){
+        const G1=false;
+        const G2=false;
+        const G3=false;
+        const G4=true;
+    }
     return (
         <div className="background_map">
             <div>
-            <img src={y_1} className="map_1"/>
-            <img src={b_2} className="map_2"/>
-            <img src={b_3} className="map_3"/>
-            <img src={b_4} className="map_4"/>
+                <div>
+                    {G1
+                        ? <img src={y_1} onClick={s_1} className="map_1"/>
+                        : <img src={b_1} onClick={s_1} className="map_1"/>}
+                </div>
+                <div>
+                    {G2
+                        ? <img src={y_2} onClick={s_2} className="map_2"/>
+                        : <img src={b_2} onClick={s_2} className="map_2"/>}
+                </div>
+                <div>
+                    {G3
+                        ? <img src={y_3} onClick={s_3} className="map_3"/>
+                        : <img src={b_3} onClick={s_3} className="map_3"/>}
+                </div>
+                <div>
+                    {G4
+                        ? <img src={y_4} onClick={s_4} className="map_4"/>
+                        : <img src={b_4} onClick={s_4} className="map_4"/>}
+                </div>
             </div>
             <div>
             <Link to={"/list"}>
