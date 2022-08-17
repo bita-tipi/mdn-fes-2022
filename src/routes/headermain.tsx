e726f748832e929fb681c01022a144c0abfc4a83
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-08-17 10:27:03 +0900

    home_menu

--- src/routes/headermain.tsx
+++ src/routes/headermain.tsx
@@ -4,6 +4,7 @@ import './header.css';
 import { Link, NavLink, useHistory } from "react-router-dom";
 import backbottun from "../assets/img/backbottun.png"
 import header_main from "../assets/img/header_main.png"
+import menu from "../assets/img/home_menu.svg"
 
 export default function Header() {
     function handleClick() {
@@ -14,6 +15,9 @@ export default function Header() {
       <header className="header">
             <img src={backbottun} onClick={handleClick} className="back_button"/>
             <img src={header_main} className="header_main"/>
+            <Link to={"/"}>
+            <img src={menu}className="menu"/>
+            </Link>
       </header>
     )
   }
