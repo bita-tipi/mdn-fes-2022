fa8e6728e44c4c2a84d0838882d8069ab3002a2f
wappon_28_dev <wappon_28_dev@ruru.be>
2022-08-21 21:16:00 +0900

    階の選択ボタンのソースを改善したよ

--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -4,79 +4,45 @@ import map_list from "../../assets/img/map_list.svg";
 import "./map.css";
 import { Link } from "react-router-dom";
 import Header from "../header/headermain";
-import b_1 from "../../assets/img/map_1_b.svg";
-import y_1 from "../../assets/img/map_1_y.svg";
-import y_2 from "../../assets/img/map_2_y.svg";
-import b_2 from "../../assets/img/map_2_b.svg";
-import y_3 from "../../assets/img/map_3_y.svg";
-import b_3 from "../../assets/img/map_3_b.svg";
-import y_4 from "../../assets/img/map_4_y.svg";
-import b_4 from "../../assets/img/map_4_b.svg";
+import f1_none from "../../assets/img/map_1_b.svg";
+import f1_hovered from "../../assets/img/map_1_y.svg";
+import f2_none from "../../assets/img/map_2_b.svg";
+import f2_hovered from "../../assets/img/map_2_y.svg";
+import f3_none from "../../assets/img/map_3_b.svg";
+import f3_hovered from "../../assets/img/map_3_y.svg";
+import f4_none from "../../assets/img/map_4_b.svg";
+import f4_hovered from "../../assets/img/map_4_y.svg";
+
+const selectorStruct = [
+    [f1_none, f2_none, f3_none, f4_none],
+    [f1_hovered, f2_hovered, f3_hovered, f4_hovered],
+];
 
 function Map() {
-    let [G1] = useState(1);
-    let [G3] = useState(0);
-    let [G2] = useState(0);
-    let [G4] = useState(0);
-    function s_1() {
-        let G1 = 1;
-        let G2 = 0;
-        let G3 = 0;
-        let G4 = 0;
-        alert("a");
+    const [currentFloorIndex, updateFloorIndex] = useState(0);
+
+    function floorSelector() {
+        let selector = [];
+        for (let index = 0; index < selectorStruct[0].length; index++) {
+            const isSelected = currentFloorIndex === index;
+            selector.push(
+                <div>
+                    <img
+                        src={selectorStruct[Number(isSelected)][index]}
+                        onClick={() => updateFloorIndex(index)}
+                        className={"floor_selector f" + (index + 1)}
+                        alt=""
+                    />
+                </div>
+            );
+        }
+        return selector;
     }
-    function s_2() {
-        let G1 = 0;
-        let G2 = 1;
-        let G3 = 0;
-        let G4 = 0;
-        alert("a");
-    }
-    function s_3() {
-        let G1 = 0;
-        let G2 = 0;
-        let G3 = 1;
-        let G4 = 0;
-        alert("a");
-    }
-    function s_4() {
-        let G1 = 0;
-        let G2 = 0;
-        let G3 = 0;
-        let G4 = 1;
-    }
+
     return (
         <div className="background_map">
-            <div>
-                <div>
-                    {G1 ? (
-                        <img src={y_1} onClick={s_1} className="map_1" />
-                    ) : (
-                        <img src={b_1} onClick={s_1} className="map_1" />
-                    )}
-                </div>
-                <div>
-                    {G2 ? (
-                        <img src={y_2} onClick={s_2} className="map_2" />
-                    ) : (
-                        <img src={b_2} onClick={s_2} className="map_2" />
-                    )}
-                </div>
-                <div>
-                    {G3 ? (
-                        <img src={y_3} onClick={s_3} className="map_3" />
-                    ) : (
-                        <img src={b_3} onClick={s_3} className="map_3" />
-                    )}
-                </div>
-                <div>
-                    {G4 ? (
-                        <img src={y_4} onClick={s_4} className="map_4" />
-                    ) : (
-                        <img src={b_4} onClick={s_4} className="map_4" />
-                    )}
-                </div>
-            </div>
+            <div>{floorSelector()}</div>
+            <p>いまは{currentFloorIndex + 1}F を選択してるよ</p>
             <div>
                 <Link to={"/list"}>
                     <img src={map_list} className="list" />
--- src/routes/top_back/top_back.tsx
+++ src/routes/top_back/top_back.tsx
@@ -1,36 +1,34 @@
-import { url } from "inspector";
 import React from "react";
-import logo from "../logo.svg";
-import top_center from "../assets/img/top_center.svg"
-import top_link from "../assets/img/top_link.svg"
-import under_back from "../assets/img/under_back.svg"
-import under_center from "../assets/img/under_center.svg"
-import hata from "../assets/img/hata.svg"
-import './top_back.css';
-import { Link, NavLink } from "react-router-dom";
-import under_moji from "../assets/img/under_moji.svg"
-import under_bottom_moji from "../assets/img/under_bottom_moji.svg"
-import under_obi from "../assets/img/under_obi.svg"
-import schedule from "../assets/img/schedule.svg"
+import top_link from "../../assets/img/top_link.svg";
+import under_center from "../../assets/img/under_center.svg";
+import hata from "../../assets/img/hata.svg";
+import "./top_back.css";
+import { Link } from "react-router-dom";
+import under_moji from "../../assets/img/under_moji.svg";
+import under_bottom_moji from "../../assets/img/under_bottom_moji.svg";
+import under_obi from "../../assets/img/under_obi.svg";
+import schedule from "../../assets/img/schedule.svg";
 
 function Top_back() {
     return (
         <div>
             <header className="top_back">
                 <div>
-                <img src={under_center}className="under_center"/>
-                <Link to={`/contents`}>
-                    <img src={top_link}className="under_link"/>
-                </Link>
-                <img src={under_moji}className="under_moji"/>
-                <img src={under_bottom_moji}className="under_bottom_moji"/>
-                <img src={hata}className="hata"/>
-                <img src={under_obi}className="under_obi"/>
-                <Link to={"/"}>
-                        <img src={schedule} className="back_schedule"/>
+                    <img src={under_center} className="under_center" />
+                    <Link to={`/contents`}>
+                        <img src={top_link} className="under_link" />
+                    </Link>
+                    <img src={under_moji} className="under_moji" />
+                    <img
+                        src={under_bottom_moji}
+                        className="under_bottom_moji"
+                    />
+                    <img src={hata} className="hata" />
+                    <img src={under_obi} className="under_obi" />
+                    <Link to={"/"}>
+                        <img src={schedule} className="back_schedule" />
                     </Link>
                 </div>
-                
             </header>
         </div>
     );
