753d26d7aca00f359cd107296ff08498d87c6ff1
wappon_28_dev <wappon_28_dev@ruru.be>
2022-09-03 18:27:36 +0900

    ヘッダーと目次のスタイル修正 (#5)

--- src/routes/header/headermain.tsx
+++ src/routes/header/headermain.tsx
@@ -4,22 +4,38 @@ import { Link } from "react-router-dom";
 import backbottun from "../../assets/img/backbottun.png";
 import header_main from "../../assets/img/header_main.png";
 import menu from "../../assets/img/home_menu.svg";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const shouldHideHeaderRouterList = ["/"];
 
 export default function Header() {
     const navigate = useNavigate();
+    const router = useLocation();
 
-    return (
-        <header className="header">
-            <img
-                src={backbottun}
-                onClick={() => navigate(-1)}
-                className="back_button"
-            />
-            <img src={header_main} className="header_main" />
-            <Link to={"/"}>
-                <img src={menu} className="menu" />
-            </Link>
+    const shouldHideHeader = shouldHideHeaderRouterList.every(
+        (val) => val === router.pathname
+    );
+    const shouldMakeGoldenHeader = router.pathname === "/koutyou";
+
+    const headerStyle = {
+        backgroundColor: shouldMakeGoldenHeader ? "#ebebd4" : "#eeeeee",
+    };
+
+    return !shouldHideHeader ? (
+        <header className="header" style={headerStyle}>
+            <div className="header-container">
+                <img
+                    src={backbottun}
+                    onClick={() => navigate(-1)}
+                    className="back_button"
+                />
+                <img src={header_main} className="header_main" />
+                <Link to={"/"}>
+                    <img src={menu} className="menu" />
+                </Link>
+            </div>
         </header>
+    ) : (
+        <div></div>
     );
 }
