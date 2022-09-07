49a42f92c1fb06d28097c98044482fe0ded593a0
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-09-07 16:59:46 +0900

    tyyuui

--- src/routes/tyui/tyui.tsx
+++ src/routes/tyui/tyui.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import left_over from "../../assets/img/left_over.svg";
+import left_over from "../../assets/img/tyui_left_over.svg";
 import back from "../../assets/img/tyui_background.svg";
-import right_over from "../../assets/img/right_over2.svg";
+import right_over from "../../assets/img/tyui_right_over.svg";
 import left_bottom from "../../assets/img/tyui_left_bottom.svg";
 import right_bottom from "../../assets/img/tyui_right_bottom.svg";
 import "./tyui.css";
