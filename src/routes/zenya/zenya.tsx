7f948383b98c791a0b414c7ebddfc9936bda0f40
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-09-12 17:33:43 +0900

    kouya

--- src/routes/zenya/zenya.tsx
+++ src/routes/zenya/zenya.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "../header/headermain";
 import "./zenya.css";
-import schedule from "../../assets/img/zenya_shedule.svg";
-import time from "../../assets/img/zenya_time.svg";
+import bar from "../../assets/img/kouya_bar.svg";
 import title from "../../assets/img/zenya_title.svg";
 
 function Zenya() {
     return (
         <body className="background_zenya">
+            <div className="kouya">
                 <img src={title} className="title_zenya"/>
-                <img src={time} className="time_zenya"/>
-                <img src={schedule}className="schedule_zenya"/>
+                <img src={bar} className="kouya_bar"/>
+            </div>
         </body>
     );
 }
