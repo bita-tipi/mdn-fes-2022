081cda70fa213e77c427ae0e9248654988ce7935
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-09-14 17:51:19 +0900

    koutyou

--- src/routes/koutyou/koutyou.tsx
+++ src/routes/koutyou/koutyou.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./koutyou.css";
-import setumei from "../../assets/img/koutyou_setumei.svg";
+import setumei from "../../assets/img/koutyo_setumei.svg";
 import syasin from "../../assets/img/koutyou_syasin.svg";
 
 function Koutyou() {
     return (
         <body className="background_koutyou">
             <div className="Kty">
-                <img src={syasin} className="syasin" alt="syasin" />
                 <img
                     src={setumei}
                     className="koutyousetumei"
