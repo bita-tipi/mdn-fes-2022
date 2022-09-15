ff5bc92aed836ec79f8038b1eb52dc2a3ba95a9d
ta-nu-13 <yuuuuuuuuuuka_718@au.com>
2022-09-16 01:31:01 +0900

    stamp

--- src/routes/stamp_rally/stamp_rally.tsx
+++ src/routes/stamp_rally/stamp_rally.tsx
@@ -1,44 +1,112 @@
 import React from "react";
 import { useState } from "react";
-import stamp_camera from "../../assets/img/stamp_camera.svg";
 import back from "../../assets/img/background_stamp.svg";
-import memo from "../../assets/img/stamp_memo.svg";
 import bingo from "../../assets/img/stamp_setumei.svg";
 import stamp1 from "../../assets/img/AMP.svg";
-import stamp2 from "../../assets/img/earth.svg";
-import stamp3 from "../../assets/img/gun.svg";
+import stamp2 from "../../assets/img/locket.svg";
+import stamp3 from "../../assets/img/earth.svg";
 import stamp4 from "../../assets/img/lens.svg";
-import stamp5 from "../../assets/img/umbrella.svg";
+import stamp5 from "../../assets/img/lanthanum.svg";
 import stamp6 from "../../assets/img/shoes.svg";
 import stamp7 from "../../assets/img/gun.svg";
-import stamp8 from "../../assets/img/gun.svg";
-import stamp9 from "../../assets/img/gun.svg";
+import stamp8 from "../../assets/img/bell.svg";
+import stamp9 from "../../assets/img/umbrella.svg";
+import yellowCell from "../../assets/img/back_y.svg";
+import pinkCell from "../../assets/img/back_p.svg";
 import "./stamp_rally.css";
-import Header from "../header/headermain";
-import { classDataType, CLASS_DATA } from "../../assets/data/constants";
-import {useParams} from 'react-router-dom';
-import {hashTable} from "../../model/hash-table"
-import {getStampIndex} from "../../model/hash-table"
+import { assert } from "../../assets/data/constants";
+import { useLocation } from "react-router-dom";
+import { getStampIndex } from "../../model/hash-table";
 
 
-const stamp = [
-    [stamp1, stamp2, stamp3, stamp4, stamp5, stamp6, stamp7, stamp8, stamp9],
+const stamps = [
+    stamp1,
+    stamp2,
+    stamp3,
+    stamp4,
+    stamp5,
+    stamp6,
+    stamp7,
+    stamp8,
+    stamp9,
 ];
 
-
+const initShowedStamp = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+];
 function Stamp_rally() {
-    const hashParams = useParams()
-    const [currentStampCell, updateStampCell] = useState(0);
+    //
+    //
+    // reactHooks definition; do not remove
+    const [currentShowedStamp, updateShowedStamp] = useState(initShowedStamp);
 
-console.log(hashTable.get("a"));
+    // initShowedStampの引数のtruthyIndex番目をtrueにするよ
+    function updateIndexedShowedStamp2Truthy(truthyIndex: number) {
+        assert(Number.isInteger(truthyIndex));
+        const deepCopiedList = JSON.parse(JSON.stringify(currentShowedStamp));
+        deepCopiedList[truthyIndex] = true;
+        updateShowedStamp(deepCopiedList);
+    }
+    // -- definition end --
+
+    const location = useLocation().search;
+    const wholeParam = new URLSearchParams(location);
+    const hashParam = wholeParam.get("c");
+    console.log(`hashParam => ${hashParam}`);
+
+    // パラメーターが存在したかの確認 -> スタンプラリーの処理はじめ
+    if (hashParam !== null) {
+        const stampIndex = getStampIndex(hashParam);
+
+        // stampIndex番目のinitShowedStampをtrueをするよ
+        updateIndexedShowedStamp2Truthy(stampIndex);
+    }
+    const getStampElement = () => {
+        let stampElement = [];
+
+        for (let index = 0; index < stamps.length; index++) {
+            stampElement.push(
+                <div>
+                    {
+                        // セル (背景の枠) の表示
+                        <img
+                            src={index % 2 === 0 ? pinkCell : yellowCell}
+                            alt="cell"
+                            className="back_stamp"
+                        />
+                    }
+                    {
+                        // スタンプの表示
+                        currentShowedStamp[index] ? (
+                            <img src={stamps[index]} alt="stamps" className="back_stamp"/>
+                        ) : (
+                            <></>
+                        )
+                    }
+                    {
+                        // 3回に1回改行を挿入
+                        (index + 1) % 3 === 0 ? <br /> : <></>
+                    }
+                </div>,
+            );
+        }
+        return stampElement;
+    };
 
     return (
         <body className="background_stamp">
-            <header className="stamp_main">
-                <img src={back} className="stamp_back" alt="stamp_back" />
+            <div className="stamp_main">
+                <div className="flex">{getStampElement()}</div>
                 <img src={bingo} className="bingo" alt="bingo" />
-            </header>
-            <div></div>
+            </div>
         </body>
     );
 }
